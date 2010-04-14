4a5a97db4554cf13360a1f8c1806bcc2c114259b
nithin stany dsouza <nithinstany@gmail.com>
2010-04-14 15:25:04 +0530

    unused deleted

--- db/migrate/20100325100648_add_custom_description_field_to_categories_wish_lists.rb
+++
@@ -1,9 +0,0 @@
-class AddCustomDescriptionFieldToCategoriesWishLists < ActiveRecord::Migration
-  def self.up
-    add_column :categories_wish_lists, :custom_description, :text
-  end
-
-  def self.down
-    remove_column :categories_wish_lists, :custom_description
-  end
-end
