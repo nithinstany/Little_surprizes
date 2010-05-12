4edd98389f8d66bcd57dc00349e41c7908d72ddd
nithin stany dsouza <nithinstany@gmail.com>
2010-05-12 16:38:04 +0530

    spell mistake chnages

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,7 +8,7 @@ class Order < ActiveRecord::Base
       self.express_payer_id = details.payer_id
       self.first_name = details.params["first_name"]
       self.last_name = details.params["last_name"]
-      self.ammount = details.params["order_total"]
+      self.amount = details.params["order_total"]
     end
   end
 
