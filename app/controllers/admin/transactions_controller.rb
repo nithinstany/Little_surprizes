19176cfd7ce08ec80627e97aa10040dfd300ebac
isiri <isiri@isiri-desktop.(none)>
2010-07-12 18:28:08 +0530

    latest changes

--- app/controllers/admin/transactions_controller.rb
+++ app/controllers/admin/transactions_controller.rb
@@ -3,7 +3,7 @@ class Admin::TransactionsController < ApplicationController
   before_filter :check_admin
   def index
    @transactions = Order.find(:all)
-   @total_amount_of_transactions = Order.find(:all,:select => 'sum(amount) as amount').first.amount.to_f unless @transactions.count>=1 
+   @total_amount_of_transactions = Order.find(:all,:select => 'sum(amount) as amount').first.amount.to_f unless @transactions.blank? 
   end
 
 end
