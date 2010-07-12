aa9947c93c7a3de88c4319c37a449c8640939f7f
isiri <isiri@isiri-desktop.(none)>
2010-07-12 18:02:54 +0530

    latest changes

--- app/controllers/admin/points_controller.rb
+++ app/controllers/admin/points_controller.rb
@@ -4,8 +4,19 @@ class Admin::PointsController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
+     fb_session = Facebooker::Session.new('d7069c71e7b928287fccf3c74f67beec', '4c425b88e6730e941276904269779024') # api key and secret
+   begin
+      fb_session.secure_with!(@user.session_key, @user.facebook_id, 2.hour.from_now)
+      @fb_user = Facebooker::User.new(@user.facebook_id, fb_session)
+       puts"SSSSSSSSSSSSSSSSSSSSSSSSSS #{@fb_user.name}"
+      rescue Facebooker::Session::SessionExpired
   end
-
+  end
+  def show
+    @user = User.find(params[:user_id])
+    @gift = Gift.find(params[:id])
+    @orders = Order.find(:all,:conditions => ['wish_list_id =?',@gift.wish_list_id])
+  end
 
 end
 
