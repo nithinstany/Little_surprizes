d81415acc42b1b49fccf5cec19536ebb24f58136
isiri <isiri@isiri-desktop.(none)>
2010-07-23 17:50:36 +0530

    gift helper

--- app/helpers/admin/gifts_helper.rb
+++ app/helpers/admin/gifts_helper.rb
@@ -1,12 +1,9 @@
 module Admin::GiftsHelper
   
   def recepient_message
-    arry = ''
+    arry = []
     @orders.each do |order|
-      unless order.payer_id.nil?
-        arry += User.find(order.payer_id).name 
-        arry += ','
-      end
+      arry << [User.find(order.payer_id).name]  unless order.payer_id.nil?
     end
     
     "<!DOCTYPE HTML PUBLIC '//W3C//DTD HTML 4.01 Transitional//EN'>
@@ -15,7 +12,7 @@ module Admin::GiftsHelper
 <body>
   Hi #{@user.name},<br />
     We at Little Surprizes would like to wish you on this very special occasion.We hope it will be most joyous and filled with pleasant surprises like this one!!!!!!!!!!!!!!! 
-#{arry} have contributed for a fantastic gift from your wishlist. <br />
+#{arry.join(', ')} have contributed for a fantastic gift from your wishlist. <br />
 Here is that Gift for you: <br />
 < Gift Card Image> with Amount/ID etc. <br />
 We hope this was a pleasant surprise for you.<br />
