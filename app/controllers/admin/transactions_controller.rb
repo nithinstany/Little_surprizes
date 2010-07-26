11c723c5274b93fc337f9d8328a95b3bc1b2e87f
isiri <isiri@isiri-desktop.(none)>
2010-07-26 11:33:23 +0530

    fee chnages tranaction

--- app/controllers/admin/transactions_controller.rb
+++ app/controllers/admin/transactions_controller.rb
@@ -6,7 +6,10 @@ class Admin::TransactionsController < ApplicationController
    @transactions = Order.find(:all)
    @total_amount_of_transactions = Order.find(:all,:select => 'sum(amount) as amount').first.amount.to_f unless @transactions.blank?
    @paypal_fees = Order.find(:all,:select => 'sum(paypal_fee) as paypal').first.paypal.to_f unless @transactions.blank?
-   @processing_fees = Order.find(:all,:select => 'sum(processing_fee) as processing_fee').first.processing_fee.to_f unless @transactions.blank?
+   processing_fees = Order.find(:all,:select => 'sum(processing_fee) as processing_fee').first.processing_fee.to_f unless @transactions.blank?
+   
+   @paypal_fees += processing_fees if processing_fees
+   
    @little_surprizes_fees = Order.find(:all,:select => 'sum(little_surprizes_fee) as little_surprizes_fee').first.little_surprizes_fee.to_f unless @transactions.blank?
    
   end
