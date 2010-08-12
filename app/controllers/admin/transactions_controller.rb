bd15453fba6396d49f36a94e32a688869d9984d8
isiri <isiri@isiri-desktop.(none)>
2010-08-12 13:07:52 +0530

    tranaction controller changes

--- app/controllers/admin/transactions_controller.rb
+++ app/controllers/admin/transactions_controller.rb
@@ -3,14 +3,14 @@ class Admin::TransactionsController < ApplicationController
   before_filter :check_admin
   
   def index
-   @transactions = Order.find(:all)
-   @total_amount_of_transactions = Order.find(:all,:select => 'sum(amount) as amount').first.amount.to_f unless @transactions.blank?
-   @paypal_fees = Order.find(:all,:select => 'sum(paypal_fee) as paypal').first.paypal.to_f unless @transactions.blank?
-   processing_fees = Order.find(:all,:select => 'sum(processing_fee) as processing_fee').first.processing_fee.to_f unless @transactions.blank?
+   @transactions = Order.find(:all,:conditions => ['status = ?', 'success'])
+   @total_amount_of_transactions = Order.find(:all,:conditions => ['status = ?', 'success'],:select => 'sum(amount) as amount').first.amount.to_f unless @transactions.blank?
+   @paypal_fees = Order.find(:all,:conditions => ['status = ?', 'success'],:select => 'sum(paypal_fee) as paypal').first.paypal.to_f unless @transactions.blank?
+   processing_fees = Order.find(:all,:conditions => ['status = ?', 'success'],:select => 'sum(processing_fee) as processing_fee').first.processing_fee.to_f unless @transactions.blank?
    
    @paypal_fees += processing_fees if processing_fees
    
-   @little_surprizes_fees = Order.find(:all,:select => 'sum(little_surprizes_fee) as little_surprizes_fee').first.little_surprizes_fee.to_f unless @transactions.blank?
+   @little_surprizes_fees = Order.find(:all,:conditions => ['status = ?', 'success'],:select => 'sum(little_surprizes_fee) as little_surprizes_fee').first.little_surprizes_fee.to_f unless @transactions.blank?
    
   end
 
